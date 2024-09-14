f1a8add0c6a6a8319f209e6e68a2375f11d139a5
Vitor de Assis <vitor.apassos@senacsp.edu.br>
2024-09-13 21:34:44 -0300

    Variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ *  Estudo de constante e variáveis
+ *  @author Vitor de Assis
+ */
+
+console.clear()
+
+// Variáveis
+let nome 
+let idade
+let vip
+
+nome = "Vitor de Assis"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 56
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
