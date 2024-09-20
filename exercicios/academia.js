694bd2019e4656ef981eb506f20bbb6cc60b3acb
Vitor de Assis <vitor.apassos@senacsp.edu.br>
2024-09-19 22:18:36 -0300

    Criado pasta EXERCÍCIOS com um arquivo de ficha de academia chamado academia.js

---
+++ exercicios/academia.js
@@ -0,0 +1,30 @@
+/**
+ * Ficha de um aluno de academia
+ * @author Vitor de Assis
+ */
+
+// variáveis
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+// entrada de dados
+nome = "Vitor de Assis"
+idade = 25
+peso = 70
+altura = 1.71
+vip = true
+
+
+// saída
+console.log(" ***Ficha do Aluno***")
+console.log("_____________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`VIP: ${vip}`)
